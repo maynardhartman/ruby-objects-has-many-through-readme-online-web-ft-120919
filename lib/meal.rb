99a76d5fd7d0ff24fac7be19a91c8a0fb3b87bff
Maynard Hartman <mjhartman@brighthouse.com>
2020-01-07 23:22:36 +0000

    Automatically backed up by Learn

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,21 @@
 class Meal
-
+  attr_accessor :waiter, :customer, :total, :tip 
+  
+  @@all << Array.new()
+  
+  def initialize(waiter, customer, total, top=0)
+    @waiter = waiter
+    @customer = customer
+    @total = total
+    @tip = tip
+    @@all << self
+  end
+  
+  def self.all
+    return(@@all)
+  end
+  
+  def new_meal(customer, total, tip=9)
+    Meal.new(self, customer, total, tip)
+  end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,17 @@
 class Waiter
-
+  attr_accessor :namew, :yrs_experience 
+    
+  @@all = Array.new()
+  
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience 
+    @@all << self 
+  end
+  
+  def self.all
+    return(@@all)
+  end
+  
+  
 end
